9c97ed89732d6329da780f63c768d06520ef6038
Nick Latham <nlatham@zagmail.gonzaga.edu>
2020-11-28 16:08:50 -0800

    fixed the runtime error in character creation scene and fixed some bugs with firebase

--- Into The Dungeon/CharacterSelectionScene.swift
+++ Into The Dungeon/CharacterSelectionScene.swift
@@ -6,74 +6,116 @@
 //  Copyright © 2020 Vy Nguyen. All rights reserved.
 //
 
-import UIKit
 import SpriteKit
 import GameplayKit
-import FirebaseDatabase
-import Firebase
 
-class CharacterSelectionController: UIViewController {
-
-    var ref: DatabaseReference!
+class CharacterSelectionScene: SKScene {
     
-    var gameID: String? = nil
-    var userName: String? = nil
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private var lastUpdateTime : TimeInterval = 0
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    var entityManager: EntityManager! // user to remove or add an entity
+    
+    
+    override func sceneDidLoad() {
+// example
+//        self.lastUpdateTime = 0
+//
+//
+//        // Get label node from scene and store it for use later
+//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+//        if let label = self.label {
+//            label.alpha = 0.0
+//            label.run(SKAction.fadeIn(withDuration: 2.0))
+//        }
+//
+//        // Create shape node to use during mouse interaction
+//        let w = (self.size.width + self.size.height) * 0.05
+//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//
+//        if let spinnyNode = self.spinnyNode {
+//            spinnyNode.lineWidth = 2.5
+//
+//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+//                                              SKAction.fadeOut(withDuration: 0.5),
+//                                              SKAction.removeFromParent()]))
+//        }
         
-        //FirebaseApp.configure()
-
-        ref = Database.database().reference()
-        
-        self.ref.child("games").setValue(1324)
-        
-        self.ref.child("games").observeSingleEvent(of: .value, with: { (sanpshot) in
-            if let id = sanpshot.value as? Int {
-                print("thh value from the database: \(id)")
-            }
-        })
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
+    // -MARK: Battle Scene
+        //
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.green
+            self.addChild(n)
         }
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.blue
+            self.addChild(n)
         }
     }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
+    
+    func touchUp(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            self.addChild(n)
+        }
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let label = self.label {
+            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        }
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        self.lastUpdateTime = currentTime
+    }
+    
+
 }
+
+
---
+++ Into The Dungeon/Entity/Guard.swift
@@ -0,0 +1,9 @@
+//
+//  Guard.swift
+//  Into The Dungeon
+//
+//  Created by Latham, Nicholas De Artola on 11/28/20.
+//  Copyright © 2020 Vy Nguyen. All rights reserved.
+//
+
+import Foundation
