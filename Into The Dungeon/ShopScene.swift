817bee17ffe285c7abb6d6d42366834662219beb
Vy Nguyen <nguyen1446@sps81.org>
2020-12-05 10:29:51 -0800

    character selection scene remove

--- Into The Dungeon/ShopScene.swift
+++ Into The Dungeon/ShopScene.swift
@@ -22,30 +22,6 @@ class ShopScene: SKScene {
     
     
     override func sceneDidLoad() {
-// example
-//        self.lastUpdateTime = 0
-//
-//
-//        // Get label node from scene and store it for use later
-//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-//        if let label = self.label {
-//            label.alpha = 0.0
-//            label.run(SKAction.fadeIn(withDuration: 2.0))
-//        }
-//
-//        // Create shape node to use during mouse interaction
-//        let w = (self.size.width + self.size.height) * 0.05
-//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-//
-//        if let spinnyNode = self.spinnyNode {
-//            spinnyNode.lineWidth = 2.5
-//
-//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-//                                              SKAction.fadeOut(withDuration: 0.5),
-//                                              SKAction.removeFromParent()]))
-//        }
-        
     // -MARK: Battle Scene
         //
     }
