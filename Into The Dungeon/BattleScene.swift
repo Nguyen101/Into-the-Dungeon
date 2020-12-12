c85bc7af39627349a4e7a73986c8d5cbaf269ed0
Nick Latham <nlatham@zagmail.gonzaga.edu>
2020-12-12 11:35:50 -0800

    feat: added a list of player nodes to the battle scene

--- Into The Dungeon/BattleScene.swift
+++ Into The Dungeon/BattleScene.swift
@@ -19,7 +19,9 @@ class BattleScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
-    var players: [String] = []
+    var players: [Player] = []
+        
+    var playerNames: [String] = []
     var turn: Int = 0
     
 //    var entityManager: EntityManager! // user to remove or add an entity
@@ -39,7 +41,7 @@ class BattleScene: SKScene {
                 print("couldn't get players")
                 return
             }
-            self.players = players
+            self.playerNames = players
             
             guard let turn = data["turn"] as? Int else {
                 print("couldnt get turn")
@@ -47,8 +49,24 @@ class BattleScene: SKScene {
             }
             self.turn = turn
             
+            self.deleteAllPlayerNodes()
+            
             for x in players {
                 if let player = data[x] as? NSDictionary {
+                    
+                    if let className = player["class"] as? String {
+                        if className == "Cleric" {
+                            self.players.append(Player(playerType: .cleric))
+                        }else if className == "Mage" {
+                            self.players.append(Player(playerType: .mage))
+                        }else if className == "Archer" {
+                            self.players.append(Player(playerType: .archer))
+                        }else if className == "Warrior" {
+                            self.players.append(Player(playerType: .warrior))
+                        }
+                    }
+                    
+                    self.addPlayerNodes()
                     
                     if let cards = player["cards"] as? String {
                         
@@ -58,23 +76,54 @@ class BattleScene: SKScene {
                 }
             }
             
+            
+            
         }
+    }
+    
+    func addPlayerNodes(){
+        var i = 0
+        for x in players {
+            x.name = playerNames[i]
+            x.size = CGSize(width: x.size.width/8, height: x.size.height/8)
+            x.position = CGPoint(x: 0, y: i*10)
+            i += 1
+            addChild(x)
+        }
+    }
+    
+    /*
+     deletes all the nodes
+     */
+    func deleteAllPlayerNodes(){
+        for x in self.players {
+            x.removeFromParent()
+        }
+        self.players = []
     }
     
     /*
      sets the turn to be the next players
      */
     func nextPlayersTurn(){
-        turn = (turn + 1) % players.count
+        updatePlayerInDatabase()
+        turn = (turn + 1) % playerNames.count
         FirebaseUtils.setPlayerTurn(gameID: FirstScreenViewController.gameID, turn: turn)
+    }
+    
+    /*
+     updates the player in the database
+     */
+    func updatePlayerInDatabase(){
+        
     }
     
     /*
      checks to see if it is the current players turn
      */
     func isCurrentPlayersTurn() -> Bool{
-        if turn < players.count {
-            if players[turn] == FirstScreenViewController.userName {
+        if turn < playerNames.count {
+            if playerNames[turn] == FirstScreenViewController.userName {
                 return true
             }
         }
